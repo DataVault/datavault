717166173a6f7fdac13f2d7df5df2208a2d7d929
Tom Higgins <thomas.higgins@manchester.ac.uk>
2015-06-02 11:29:44 +0100

    Demo end-to-end process (broker->worker)

--- datavault-worker/src/main/java/org/datavault/worker/queue/Receiver.java
+++ datavault-worker/src/main/java/org/datavault/worker/queue/Receiver.java
@@ -34,9 +34,48 @@ public class Receiver {
             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
             String message = new String(delivery.getBody());
             System.out.println(" [x] Received '" + message + "'");
-            
+
             // Dispatch the job ...
-            
+
+            // Just testing ...
+
+            try {
+                String archiveBase = "/Users/tom/datavault/archive";
+                String tempBase = "/Users/tom/datavault/temp";
+
+                java.io.File inputFile = new java.io.File(message);
+                if (inputFile.exists()) {
+
+                    // Create a new directory based on a UUID
+                    java.util.UUID bagID = java.util.UUID.randomUUID();
+                    java.nio.file.Path bagPath = java.nio.file.Paths.get(tempBase, bagID.toString());
+                    java.io.File bagDir = bagPath.toFile();
+                    bagDir.mkdir();
+
+                    // Copy the target file to the bag directory
+                    String fileName = inputFile.getName();
+                    java.nio.file.Path outputPath = bagPath.resolve(fileName);
+                    org.apache.commons.io.FileUtils.copyFile(inputFile, outputPath.toFile());
+
+                    // Bag the directory in-place
+                    org.datavault.worker.operations.Packager.createBag(bagDir);
+
+                    // Tar the bag directory
+                    String tarFileName = bagID + ".tar";
+                    java.nio.file.Path tarPath = java.nio.file.Paths.get(tempBase).resolve(tarFileName);
+                    java.io.File tarFile = tarPath.toFile();
+                    org.datavault.worker.operations.Tar.createTar(bagDir, tarFile);
+
+                    // Copy the resulting tar file to the archive area
+                    java.nio.file.Path archivePath = java.nio.file.Paths.get(archiveBase).resolve(tarFileName);
+                    org.apache.commons.io.FileUtils.copyFile(tarFile, archivePath.toFile());
+
+                } else {
+                    System.err.println("File does not exist.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         
         // Unreachable - this demo never terminates
