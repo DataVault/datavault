0f711cd2b23a29b7d9f52fd706dd268b3b18e002
Tom Higgins <thomas.higgins@manchester.ac.uk>
2015-06-23 15:50:07 +0100

    Simple withdraw implementation (tar file)

--- datavault-worker/src/main/java/org/datavault/worker/jobs/Withdraw.java
+++ datavault-worker/src/main/java/org/datavault/worker/jobs/Withdraw.java
@@ -1,8 +1,13 @@
 package org.datavault.worker.jobs;
 
 import java.util.Map;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.datavault.common.job.Context;
 import org.datavault.common.job.Job;
+import org.apache.commons.io.FileUtils;
 
 public class Withdraw extends Job {
     
@@ -17,5 +22,30 @@ public class Withdraw extends Job {
         
         System.out.println("\tbagID: " + bagID);
         System.out.println("\twithdrawalPath: " + withdrawalPath);
+        
+        try {
+            Path basePath = Paths.get(context.getActiveDir());
+            Path withdrawPath = basePath.resolve(withdrawalPath);
+            File withdrawDir = withdrawPath.toFile();
+
+            if (!withdrawDir.exists() || !withdrawDir.isDirectory()) {
+                // Target path must exist and be a directory
+                System.out.println("\tTarget directory not found!");
+            }
+
+            // Find the archived data
+            // TODO: a filesystem driver should transform a bagID to an actual path
+            String tarFileName = bagID + ".tar";
+            Path archivePath = Paths.get(context.getArchiveDir()).resolve(tarFileName);
+            File archiveFile = archivePath.toFile();
+
+            // Copy the tar file to the withdrawal area
+            System.out.println("\tCopying tar file from archive ...");
+            File withdrawFile = withdrawPath.resolve(tarFileName).toFile();
+            FileUtils.copyFile(archiveFile, withdrawFile);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
