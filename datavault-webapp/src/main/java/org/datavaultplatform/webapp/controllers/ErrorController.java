dca117cfa49b7168d79f1ad293fccf8df90fa2a1
Stuart Lewis <stuart@stuartlewis.com>
2015-10-02 19:15:28 +0100

    Stack trace and 404

--- datavault-webapp/src/main/java/org/datavaultplatform/webapp/controllers/ErrorController.java
+++ datavault-webapp/src/main/java/org/datavaultplatform/webapp/controllers/ErrorController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.MessageFormat;
 
 /**
@@ -20,27 +22,29 @@ class ErrorController {
         // Retrieve some useful information from the request
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
-        String exceptionMessage = getExceptionMessage(throwable, statusCode);
+        String exceptionMessage = getExceptionMessage(throwable, statusCode, response);
 
         String requestUri = (String) request.getAttribute("javax.servlet.error.request_uri");
         if (requestUri == null) {
             requestUri = "Unknown";
         }
 
-        String message = MessageFormat.format("{0} returned for {1} with message {2}",
-                statusCode, requestUri, exceptionMessage
-        );
+        String message = MessageFormat.format("Error code {0} returned for {1} with message:<br/> {2}",
+                                              statusCode, requestUri, exceptionMessage);
 
         model.addAttribute("message", message);
         return "error/error";
     }
 
-    private String getExceptionMessage(Throwable throwable, Integer statusCode) {
+    private String getExceptionMessage(Throwable throwable, Integer statusCode, HttpServletResponse response) {
         if (throwable != null) {
-            String reason = throwable.getCause().getMessage();
-            if (reason != null) return reason;
+            StringWriter reason = new StringWriter();
+            throwable.printStackTrace(new PrintWriter(reason));
+            response.setStatus(500);
+            if (reason != null) return reason.toString();
         }
         HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+        response.setStatus(statusCode);
         return httpStatus.getReasonPhrase();
     }
 }
