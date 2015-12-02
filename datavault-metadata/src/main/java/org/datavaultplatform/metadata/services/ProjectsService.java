54c492baca0dcd6f2425d6e3ad82fc942c0325fc
Tom Higgins <thomas.higgins@manchester.ac.uk>
2015-12-02 11:02:50 +0000

    Add skeleton metadata service API
    API for project data (e.g. from a CRIS system).

---
+++ datavault-metadata/src/main/java/org/datavaultplatform/metadata/services/ProjectsService.java
@@ -0,0 +1,13 @@
+package org.datavaultplatform.metadata.services;
+
+import org.datavaultplatform.common.metadata.Project;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ProjectsService {
+
+    public List<Project> getProjects() {
+        return new ArrayList();
+    }
+}
