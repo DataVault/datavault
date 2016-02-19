be0b2d14141ba8f0f18a9c3395eb78d083ffd7c2
Tom Higgins <thomas.higgins@manchester.ac.uk>
2016-02-19 15:40:14 +0000

    Revert "Return appropriate http status codes from Broker authentication"

--- datavault-broker/src/main/java/org/datavaultplatform/broker/authentication/RestAuthenticationFilter.java
+++ datavault-broker/src/main/java/org/datavaultplatform/broker/authentication/RestAuthenticationFilter.java
@@ -1,53 +1,23 @@
 package org.datavaultplatform.broker.authentication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedCredentialsNotFoundException;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
+ * Under the covers this builds an PreAuthenticatedAuthenticationToken which is passed to the AuthenticationManager
+ * and then on to the AuthenticationProvider(s).
+ *
  * User: Robin Taylor
- * Date: 18/02/2016
- * Time: 10:45
+ * Date: 13/11/2015
+ * Time: 13:16
  */
-public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+public class RestAuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(RestAuthenticationFilter.class);
-
-    // Can be overridden in Spring config
-    private String principalRequestHeader = "X-UserID";
-
-
-    public RestAuthenticationFilter() {
-        // Doesn't matter what goes in here as it should be overridden in the Spring config
-        super("/dummy");
-    }
-
-
-    public void setPrincipalRequestHeader(String principalRequestHeader) {
-        this.principalRequestHeader = principalRequestHeader;
-    }
-
-
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
-            throws AuthenticationException, IOException, ServletException {
-
-        Object principal = getPreAuthenticatedPrincipal(request);
-        Object credentials = getPreAuthenticatedCredentials(request);
-
-        PreAuthenticatedAuthenticationToken authRequest = new PreAuthenticatedAuthenticationToken(principal, credentials);
-        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
-        return this.getAuthenticationManager().authenticate(authRequest);
-
-    }
+    // todo : set both of these in Spring config?
+    private String UserIdRequestHeader = "X-UserID";
+    private boolean exceptionIfHeaderMissing = true;
 
     /**
      * Read and returns the header named by {@code apiKeyRequestHeader} from the
@@ -56,8 +26,15 @@ public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFi
      * @throws PreAuthenticatedCredentialsNotFoundException if the header is missing and
      * {@code exceptionIfHeaderMissing} is set to {@code true}.
      */
-    private Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-        return request.getHeader(principalRequestHeader);
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        String principal = request.getHeader(UserIdRequestHeader);
+
+        if (principal == null && exceptionIfHeaderMissing) {
+            throw new PreAuthenticatedCredentialsNotFoundException(UserIdRequestHeader
+                    + " header not found in request.");
+        }
+
+        return principal;
     }
 
     /**
@@ -65,7 +42,8 @@ public class RestAuthenticationFilter extends AbstractAuthenticationProcessingFi
      * set, this will be read and used as the credentials value. Otherwise a dummy value
      * will be used.
      */
-    private Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
         return "N/A";
     }
+
 }
