d90b585e11bc60f78ba7d83de27169dbc751a231
Robin Taylor <robin.taylor@ed.ac.uk>
2016-04-29 15:59:57 +0100

    First stab at adding a startup script for the workers

--- datavault-assembly/src/datavault-home/bin/start-worker.sh
+++ datavault-assembly/src/datavault-home/bin/start-worker.sh
@@ -1,1 +1,13 @@
-# This is a wee shell script to strat the work, only I haven't written it yet....
+#!/bin/bash
+
+if [ -z "$DATAVAULT_HOME" ]; then
+    echo "DATAVAULT_HOME is not set"
+    exit 1
+fi
+
+cd ${DATAVAULT_HOME}/lib
+
+java -cp datavault-worker-1.0-SNAPSHOT.jar:./* org.datavaultplatform.worker.WorkerManager &
+
+
+
