1f02e9e47baffb32c399e4ac1b0038f31a7ec29d
Robin Taylor <robin.taylor@ed.ac.uk>
2016-05-03 10:09:52 +0100

    Back out start-worker.sh edit for now

--- datavault-assembly/src/datavault-home/bin/start-worker.sh
+++ datavault-assembly/src/datavault-home/bin/start-worker.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-if [ -z "$DATAVAULT_HOME" ]; then
-    echo "DATAVAULT_HOME is not set"
-    exit 1
-fi
-
-cd ${DATAVAULT_HOME}/lib
-
-java -cp datavault-worker-1.0-SNAPSHOT.jar:./* org.datavaultplatform.worker.WorkerManager &
-
+# This is a wee shell script to strat the work, only I haven't written it yet....
 
 
