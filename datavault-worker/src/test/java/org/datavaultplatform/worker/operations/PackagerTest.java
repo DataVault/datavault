546a0f6eefef04169fc0ce8d97af00820c3e1296
George Hamilton <gmh04@netscape.net>
2017-10-27 17:03:13 +0100

    check manifest has expected files #381

--- datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
+++ datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
@@ -1,10 +1,13 @@
 package org.datavaultplatform.worker.operations;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,7 +28,7 @@ public class PackagerTest {
                 File.separator + "test" + File.separator + "resources";
 
         packagerResources = resourcesDir + File.separator + "packager";
-        testDir = new File(resourcesDir + File.separator + "packager-output");
+        testDir = new File(resourcesDir, "packager-output");
     }
 
     @Before
@@ -47,18 +50,18 @@ public class PackagerTest {
         // files expected in bag
         final List<String> expectFiles = Arrays.asList(
                 "bagit.txt",
-                "item.pdf",
+                TEST_FILE,
                 "bag-info.txt",
                 "manifest-md5.txt",
                 "tagmanifest-md5.txt",
                 "bagit.txt");
 
-        File dir =  new File(testDir + File.separator + "createbag");
+        File dir =  new File(testDir, "createbag");
         dir.mkdir();
 
         try{
             FileUtils.copyFileToDirectory(
-                    new File(packagerResources + File.separator + TEST_FILE),
+                    new File(packagerResources, TEST_FILE),
                     dir);
         }
         catch(IOException ex){
@@ -84,25 +87,47 @@ public class PackagerTest {
 
     @Test
     public void testValidateBag() { 
-        final String TEST_FILE = "item.pdf";
-        File dir =  new File(testDir + File.separator + "createbag");
-        dir.mkdir();
-    
+        final String TEST_FILE1 = "item.pdf";
+        final String TEST_FILE2 = "banjo.jpg";
+        File parentDir =  new File(testDir, "validatebag");
+        parentDir.mkdir();
+        File childDir =  new File(parentDir, "childdir");
+        
         try{
             FileUtils.copyFileToDirectory(
-                    new File(packagerResources + File.separator + TEST_FILE),
-                    dir);
+                    new File(packagerResources, TEST_FILE1),
+                    parentDir);
+            File test2file = new File(packagerResources, TEST_FILE2);
+            FileUtils.copyFileToDirectory(test2file, childDir);
+            
+            // create symlink in parent file to file in child
+            /*System.setProperty("user.dir", parentDir.getAbsolutePath());
+            Files.createSymbolicLink(
+                    Paths.get(TEST_FILE2),
+                    Paths.get("childdir/" + TEST_FILE2));*/
         }
         catch(IOException ex){
+            ex.printStackTrace();
             fail(ex.getMessage());
         }
         
         try{
-            File bag = new File("/mnt/dataexchange/test/bagit");
-            Packager.createBag(bag);
-            assertTrue(Packager.validateBag(bag));
+            Packager.createBag(parentDir);
+            assertTrue(Packager.validateBag(parentDir));
+
+            // #1 check files in manifest 
+            List<String> lines = FileUtils.readLines(new File(parentDir.getAbsolutePath() +  File.separator + "manifest-md5.txt"));
+            
+            // the order of files in manifest may change
+            String fPath1 = lines.get(0).split("  ")[1];
+            String fPath2 = lines.get(1).split("  ")[1];
+            assertTrue(fPath1.endsWith(TEST_FILE2));
+            assertTrue(fPath2.endsWith(TEST_FILE1));
+           
+            // #2 check symlinks 
         }
         catch(Exception ex){
+            ex.printStackTrace();
             fail(ex.getMessage());
         }
     }
@@ -115,7 +140,6 @@ public class PackagerTest {
         catch(IOException ex){
             fail(ex.getMessage());
         }
-        System.out.println("tearDown");
 
         BasicConfigurator.resetConfiguration();
     }
