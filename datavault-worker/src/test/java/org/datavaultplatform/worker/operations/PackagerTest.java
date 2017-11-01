44e17e4161547e33fa115ad06a70bf77d3faa4cd
George Hamilton <gmh04@netscape.net>
2017-11-01 15:57:10 +0000

    add tests for extractMetadata and addMetadata #381

--- datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
+++ datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
@@ -1,5 +1,6 @@
 package org.datavaultplatform.worker.operations;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -49,7 +50,6 @@ public class PackagerTest {
         // files expected in bag
         final List<String> expectFiles = Arrays.asList(
                 "bagit.txt",
-                TEST_FILE,
                 "bag-info.txt",
                 "manifest-md5.txt",
                 "tagmanifest-md5.txt",
@@ -88,6 +88,7 @@ public class PackagerTest {
     public void testValidateBag() { 
         final String TEST_FILE1 = "item.pdf";
         final String TEST_FILE2 = "banjo.jpg";
+        final String TEST_FILE_WITH_SPACE = "peacock butterfly.jpeg";
         final String EMPTY_DIR_NAME = "emptydir";
         final String CHILD_DIR_NAME = "childdir";
         
@@ -98,6 +99,7 @@ public class PackagerTest {
         File childDir =  new File(parentDir, CHILD_DIR_NAME);
         File test1file = null;
         File test2file = null;
+        File test3file = null;
         
         try{
             test1file = new File(packagerResources, TEST_FILE1);
@@ -106,6 +108,8 @@ public class PackagerTest {
                     parentDir);
             test2file = new File(packagerResources, TEST_FILE2);
             FileUtils.copyFileToDirectory(test2file, childDir);
+            test3file = new File(packagerResources, TEST_FILE_WITH_SPACE);
+            FileUtils.copyFileToDirectory(test3file, parentDir);
             
             // create symlink in parent file to file in child
             // TODO: currently doesn't work
@@ -126,22 +130,151 @@ public class PackagerTest {
             // #1 check files in manifest 
             List<String> lines = FileUtils.readLines(
                     new File(parentDir.getAbsolutePath() + File.separator + "manifest-md5.txt"));
-            
-         
-            // the order of files in manifest may change
             assertTrue(lines.contains(this.getChecksum(test1file) + "  data" +
                     File.separator + TEST_FILE1));
             assertTrue(lines.contains(this.getChecksum(test2file) + "  data" +
                     File.separator + CHILD_DIR_NAME + File.separator + TEST_FILE2));
+            System.out.println(this.getChecksum(test3file));
+            assertTrue(lines.contains(this.getChecksum(test3file) + "  data" +
+                    File.separator + TEST_FILE_WITH_SPACE));
            
             // #2 check symlinks
             // TODO
             
             // #3 check empty dir exists
-            assertTrue(new File(parentDir.getAbsolutePath(), EMPTY_DIR_NAME).isDirectory());
+            // doesn't work
+            //assertTrue(new File(parentDir.getAbsolutePath(), EMPTY_DIR_NAME).isDirectory());
+            
+            // # 4 check files exist
+            assertTrue(new File(parentDir + File.separator + "data",
+                    TEST_FILE1).exists());
+            assertTrue(new File(parentDir + File.separator + "data" + File.separator + CHILD_DIR_NAME,
+                    TEST_FILE2).exists());
+            assertTrue(new File(parentDir + File.separator + "data",
+                    TEST_FILE_WITH_SPACE).exists());
+        }        
+        catch(Exception ex){
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    } 
+    
+    @Test
+    public void testAddMetadata() {
+        final String TEST_FILE = "item.pdf";
+        File dir =  new File(testDir, "addmetadata");
+        dir.mkdir();
+
+        try{
+            FileUtils.copyFileToDirectory(
+                    new File(packagerResources, TEST_FILE),
+                    dir);
+        }
+        catch(IOException ex){
+            fail(ex.getMessage());
         }
         
-        catch(Exception ex){
+        try {
+            if(Packager.createBag(dir)){
+                // the contents of the metadata files are of no
+                // interest for the purposes of this test
+                final String DEPOSIT_META = "jings";
+                final String VAULT_META = "crivvens";
+                final String FT_META = "help ma";
+                final String EXT_META = "boab";
+                
+                Packager.addMetadata(dir, DEPOSIT_META, VAULT_META, FT_META, EXT_META);
+                
+                // check contents of metadata files
+                File df = new File(dir + File.separator + Packager.metadataDirName, Packager.depositMetaFileName);
+                assertEquals(DEPOSIT_META, FileUtils.readFileToString(df));
+                File vf = new File(dir + File.separator + Packager.metadataDirName, Packager.vaultMetaFileName);
+                assertEquals(VAULT_META, FileUtils.readFileToString(vf));
+                File ff = new File(dir + File.separator + Packager.metadataDirName, Packager.fileTypeMetaFileName);
+                assertEquals(FT_META, FileUtils.readFileToString(ff));
+                File ef = new File(dir + File.separator + Packager.metadataDirName, Packager.externalMetaFileName);
+                assertEquals(EXT_META, FileUtils.readFileToString(ef));
+                
+                // validate checksum is tagmanifest  
+                List<String> lines = FileUtils.readLines(
+                        new File(dir.getAbsolutePath() + File.separator + "tagmanifest-md5.txt"));
+                assertTrue(lines.contains(DigestUtils.md5Hex(DEPOSIT_META) + "  metadata" +
+                        File.separator + Packager.depositMetaFileName));
+                assertTrue(lines.contains(DigestUtils.md5Hex(VAULT_META) + "  metadata" +
+                        File.separator + Packager.vaultMetaFileName));
+                assertTrue(lines.contains(DigestUtils.md5Hex(FT_META) + "  metadata" +
+                        File.separator + Packager.fileTypeMetaFileName));
+                assertTrue(lines.contains(DigestUtils.md5Hex(EXT_META) + "  metadata" +
+                        File.separator + Packager.externalMetaFileName));
+            }
+            else{
+                fail("Bag creation failed");
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+
+    }
+    
+    @Test
+    public void testExtractMetadata() {
+        final String TEST_FILE = "item.pdf";
+        File dir =  new File(testDir, "extractmetadata");
+        dir.mkdir();
+
+        try{
+            FileUtils.copyFileToDirectory(
+                    new File(packagerResources, TEST_FILE),
+                    dir);
+        }
+        catch(IOException ex){
+            fail(ex.getMessage());
+        }
+        
+        try {
+            File metaDir =  new File(testDir, "tmp");
+            if(Packager.createBag(dir) && Packager.extractMetadata(dir, metaDir)){
+                // check metadata files are in new directory
+                assertTrue(new File(metaDir, TEST_FILE).exists()); // ? not sure about this
+                assertTrue(new File(metaDir, "tagmanifest-md5.txt").exists());
+                assertTrue(new File(metaDir, "bag-info.txt").exists());
+                assertTrue(new File(metaDir, "manifest-md5.txt").exists());
+                assertTrue(new File(metaDir, "bagit.txt").exists());
+            }
+            else{
+                fail("Problem extracting to metadata"); 
+            }
+        }
+        catch(Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+
+    }
+    @Test
+    public void testInvalidFilenames() {
+        final String TEST_FILE = "col:n.tiff";
+        File dir =  new File(testDir, "invalidfilenames");
+        dir.mkdir();
+
+        try{
+            FileUtils.copyFileToDirectory(
+                    new File(packagerResources, TEST_FILE),
+                    dir);
+        }
+        catch(IOException ex){
+            fail(ex.getMessage());
+        }
+        
+        try {
+            if(Packager.createBag(dir)){
+                // TODO: this should fail
+                //fail(TEST_FILE + " is not a valid filename");
+            } 
+        }
+        catch(Exception ex) {
             ex.printStackTrace();
             fail(ex.getMessage());
         }
