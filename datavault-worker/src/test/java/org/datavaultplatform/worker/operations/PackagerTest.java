5b5e68744c572e443e47a72ba46bc57927396785
George Hamilton <gmh04@netscape.net>
2017-11-02 09:55:12 +0000

    add pre-commit git hook #381

--- datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
+++ datavault-worker/src/test/java/org/datavaultplatform/worker/operations/PackagerTest.java
@@ -7,9 +7,6 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
