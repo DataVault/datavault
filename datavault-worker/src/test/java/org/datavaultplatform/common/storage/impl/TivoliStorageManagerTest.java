0d36930522355409a7b1c011a08f3bfccadad6fc
David Speed <david.speed@ed.ac.uk>
2018-01-23 12:54:19 +0000

    IssueId #399 Added more tests for the retrieve method of the TSM plugin
    1) I’ve finished the first set of store /retrieve unit tests.  All of these need the TSM client installed on the machine that is running the tests, all of the test expect to start from a ‘clean’ slate and clean up after themselves. (they don’t check if a clean failed though so you might need to delete from TSM if a previous run has gone wrong)

--- datavault-worker/src/test/java/org/datavaultplatform/common/storage/impl/TivoliStorageManagerTest.java
+++ datavault-worker/src/test/java/org/datavaultplatform/common/storage/impl/TivoliStorageManagerTest.java
@@ -2,18 +2,18 @@ package org.datavaultplatform.common.storage.impl;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.datavaultplatform.common.io.Progress;
 import org.junit.After;
 import org.junit.Before;
@@ -23,10 +23,16 @@ import org.junit.Test;
 public class TivoliStorageManagerTest {
 	
 	private TivoliStorageManager tsm = null;
+	private static String tsmResources;
+	//private static File testDir;
 	
 	@BeforeClass
 	public static void setUpClass() {
-	        
+		String resourcesDir = System.getProperty("user.dir") + File.separator + "src" +
+                File.separator + "test" + File.separator + "resources";
+
+        tsmResources = resourcesDir + File.separator + "tsm";
+        //testDir = new File(resourcesDir, "tsm-output");   
 	}
 
 	@Before
@@ -59,7 +65,7 @@ public class TivoliStorageManagerTest {
 		
 		String path = "/";
 		ProcessBuilder pb = new ProcessBuilder("dsmc", "delete", "archive", path, "-subdir=yes", "-description=" + retVal, "-noprompt");
-		System.out.println(pb.command());
+		//System.out.println(pb.command());
 
 		Process p = pb.start();
 		p.waitFor();
@@ -68,7 +74,7 @@ public class TivoliStorageManagerTest {
     @Test
     public void testStoreValidParams() {
     		Progress progress = new Progress();
-    		File working  = new File("/tmp/test.tar");
+    		File working  = new File(tsmResources, "test.tar");
     		String path = "/tmp";
     		String retVal = null;
     		try {
@@ -91,12 +97,11 @@ public class TivoliStorageManagerTest {
     @Test
     public void testStoreNonExistantFile() {
     	Progress progress = new Progress();
-		File working  = new File("/tmp/testx.tar");
+		File working  = new File(tsmResources, "testx.tar");
 		String path = "/tmp";
-		String retVal = null;
 		try {
 			// store the tar
-		retVal = tsm.store(path, working, progress);
+			tsm.store(path, working, progress);
 		// exception should be thrown by store so we should never get here
 		fail("Exception should have been thrown"); 
 		} catch (Exception e) {
@@ -107,36 +112,73 @@ public class TivoliStorageManagerTest {
     
     @Test
     public void testRetrieveValidParams() {
-    		
+	    	Progress progress = new Progress();
+		File working  = new File(tsmResources, "test.tar");
+		File temp  = new File(tsmResources, "test.tar.tmp");
+		String path = "/tmp";
+		String retVal = null;
+		try {
+			// store the tar
+			System.out.println("Store the tar");
+			retVal = tsm.store(path, working, progress);
+			//File retrieved = new File("/tmp/" + retVal);
+			assertNotNull("RetVal should not be null", retVal);
+			// check it is now in TSM
+			System.out.println("check it has been archived");
+			BufferedReader reader = this.queryArchive(working, retVal);
+			String line = null;
+			while ( (line = reader.readLine()) != null) {
+				assertFalse("Attempt to store failed", line.contains("No files matching search criteria were found"));
+			}
+			
+			// temp move the file (so we can retrieve without overwriting)
+			FileUtils.moveFile(working, temp);
+			assertFalse("The working file shouldn't exist", working.exists());
+			
+			// retrieve from TSM
+			System.out.println("Retrieve the tar");
+			//dsmc  retrieve /tmp/datavault/temp/2848@ubuntu-xenial/513c2b11-30df-4947-846b-a64309c61eb8.tar
+			tsm.retrieve(path, working, progress);
+			
+			// check that the /tmp/"retval" dir now exists
+			System.out.println("Check the retrieve archive exits");
+			assertTrue("The retrieved archive doesn't exist", working.exists());
+			
+			// delete from TSM
+			System.out.println("Delete from TSM");
+			deleteArchive(retVal);
+			
+			// delete the retrieved tar
+			System.out.println("Delete the retrieved archive");
+			Boolean deleted = working.delete();
+			assertTrue("Retrieved archive not cleaned up", deleted);
+			
+			// move the source file back
+			FileUtils.moveFile(temp, working);
+			assertTrue("The working file should exist", working.exists());
+		} catch (Exception e) {
+			fail("Unexpected exception " + e.getMessage()); 
+		}
     }
     
     @Test
     public void testRetrieveNonExistantFile() {
+    		// retrieve from TSM
+     	Progress progress = new Progress();
+    		File working  = new File("/tmp/test.tar");
+    		String path = "/tmp";
     		
+    		try {
+	    		System.out.println("Retrieve the tar");
+	    		//dsmc  retrieve /tmp/datavault/temp/2848@ubuntu-xenial/513c2b11-30df-4947-846b-a64309c61eb8.tar
+	    		tsm.retrieve(path, working, progress);
+	    		fail("Exception should have been thrown"); 
+    		} catch (Exception e) {
+    			assertNotNull("Exception should not be null", e);
+    			assertEquals("Message not as expected", "Retrieval of test.tar failed. ", e.getMessage());
+    		}
     }
     
-    @Test
-    public void testRetrieveValidParamsDuplicateFileNames() {
-    		
-    }
-    
-   
- //   @Test
-//   public void testRetrieve() {
-//    		Progress progress = new Progress();
-//    		File working  = new File("/tmp/test.tar");
-//    		String path = "/tmp";
-//
-//    		try {
-//			tsm.retrieve(path, working, progress);
-//		} catch (Exception e) {
-//			fail("Unexpected exception " + e.getMessage()); 
-//		} 
-//    		
-//    		fail("Need to test the file was retrieved to the expected location");
-//    		fail("Test me (retrieve");
-//    }
-	    
 	@After
     public void tearDown() {
 	       
