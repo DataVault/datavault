c34427e345cb0e01914923e7a32289a0d3306c1e
David Speed <david.speed@ed.ac.uk>
2018-03-15 12:51:41 +0000

    IssueId #405 Updated the plugin init to remove the error with retrieveEnabled
    1) Changed the S3 config so that retrieveEnabled is false

--- datavault-broker/src/main/java/org/datavaultplatform/broker/scheduled/InitialiseDatabase.java
+++ datavault-broker/src/main/java/org/datavaultplatform/broker/scheduled/InitialiseDatabase.java
@@ -43,7 +43,7 @@ public class InitialiseDatabase {
             HashMap<String,String> storeProperties = new HashMap<String,String>();
             storeProperties.put("rootPath", archiveDir);
             ArchiveStore tsm = new ArchiveStore("org.datavaultplatform.common.storage.impl.TivoliStorageManager", storeProperties, "Default archive store (TSM)", true);
-            ArchiveStore s3 = new ArchiveStore("org.datavaultplatform.common.storage.impl.S3Cloud", storeProperties, "Cloud archive store", true);
+            ArchiveStore s3 = new ArchiveStore("org.datavaultplatform.common.storage.impl.S3Cloud", storeProperties, "Cloud archive store", false);
             archiveStoreService.addArchiveStore(tsm);
             archiveStoreService.addArchiveStore(s3);
         }
