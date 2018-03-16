d25609890467751ed2b5b0e5195664fe0ebddf1a
William Petit <w.petit@ed.ac.uk>
2018-03-16 15:58:35 +0000

    Add a few comments in datavaultplatform.common.io.FileUtils

--- datavault-common/src/main/java/org/datavaultplatform/common/io/FileUtils.java
+++ datavault-common/src/main/java/org/datavaultplatform/common/io/FileUtils.java
@@ -1,14 +1,25 @@
 package org.datavaultplatform.common.io;
 
+/**
+ * This Class is an extension of {@link org.apache.commons.io.FileUtils} to add some util features required for the
+ * datavault project.
+ */
 public class FileUtils extends org.apache.commons.io.FileUtils {
     
     public static char[] units = new char[] { 'K','M','G','T' };
-    
+
+
+    /**
+     * Take a human readable size (i.e 1GB, 50MB, 10K) and turn it in a long representing the number of Bytes.
+     *
+     * @param humanReadableSize
+     * @return Number of Bytes
+     */
     public static long parseFormattedSizeToBytes(String humanReadableSize) {
         int identifier = -1;
-        long value = -1;
+        long value;
         int units_index = -1;
-        
+
         while ( identifier == -1 && units_index < (units.length-1) ) {
             units_index++;
             identifier = humanReadableSize.toUpperCase().indexOf(units[units_index]);
@@ -16,7 +27,7 @@ public class FileUtils extends org.apache.commons.io.FileUtils {
         
         if (identifier >= 0) {
             value = Long.parseLong(humanReadableSize.substring(0,identifier).trim());
-            
+
             boolean si = true;
             if( identifier == (humanReadableSize.length()-3) &&
                     humanReadableSize.charAt(identifier+1) == 'i' &&
