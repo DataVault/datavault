adc61aedc1a2dbbb5c8a4caa93246fd75f8b74ea
Steven Carlysle-Davies <steven.carlysle-davies@ed.ac.uk>
2018-05-07 15:51:29 +0100

    Use TransferManager for S3 operations (which should automatically retry failures etc.)

--- datavault-common/src/main/java/org/datavaultplatform/common/storage/impl/S3Cloud.java
+++ datavault-common/src/main/java/org/datavaultplatform/common/storage/impl/S3Cloud.java
@@ -17,12 +17,11 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.services.s3.transfer.Download;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 
 public class S3Cloud extends Device implements ArchiveStore {
 
@@ -30,7 +29,7 @@ public class S3Cloud extends Device implements ArchiveStore {
 	private static String defaultBucketName = "datavault-test-bucket";
 	private static String defaultRegionName = Regions.EU_WEST_1.toString();
 	public Verify.Method verificationMethod = Verify.Method.CLOUD;
-	private AmazonS3 s3;
+	private TransferManager transferManager;
 	private String bucketName;
 
 	public S3Cloud(String name, Map<String, String> config) {
@@ -53,7 +52,10 @@ public class S3Cloud extends Device implements ArchiveStore {
                 if (accessKey != null && secretKey != null) {
                 	builder = builder.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)));
                 }
-		this.s3 = builder.build();
+                this.transferManager = TransferManagerBuilder
+                                       .standard()
+                                       .withS3Client(builder.build())
+                                       .build();
 		this.bucketName = bucketName;
 	}
 
@@ -71,18 +73,8 @@ public class S3Cloud extends Device implements ArchiveStore {
 	public void retrieve(String depositId, File working, Progress progress) throws Exception {
 		logger.info("Downloading an object");
 		try {
-			S3Object object = s3.getObject(new GetObjectRequest(bucketName, depositId));
-			logger.info("Content-Type: "  + object.getObjectMetadata().getContentType());
-			S3ObjectInputStream s3is = object.getObjectContent();
-			FileOutputStream fos = new FileOutputStream(working);
-			byte[] read_buf = new byte[1024];
-		    int read_len = 0;
-		    while ((read_len = s3is.read(read_buf)) > 0) {
-		        fos.write(read_buf, 0, read_len);
-		    }
-		    s3is.close();
-		    fos.close();
-			
+			Download download = transferManager.download(bucketName, depositId, working);
+			download.waitForCompletion();
 		} catch (AmazonServiceException ase) {
 			logger.info("Caught an AmazonServiceException, which means your request made it "
                     + "to Amazon S3, but was rejected with an error response for some reason.");
@@ -92,9 +84,6 @@ public class S3Cloud extends Device implements ArchiveStore {
             logger.info("Error Type:       " + ase.getErrorType());
             logger.info("Request ID:       " + ase.getRequestId());
             throw ase;
-		} catch (FileNotFoundException fe) {
-			logger.info(fe.getMessage());
-                        throw fe;
 		} catch (AmazonClientException ace) {
 			logger.info("Caught an AmazonClientException, which means the client encountered "
                     + "a serious internal problem while trying to communicate with S3, "
@@ -110,7 +99,8 @@ public class S3Cloud extends Device implements ArchiveStore {
 		//ProgressListener listener = initProgressListener(progress, true);
 		logger.info("Uploading " + working.getName() + " to " + this.bucketName);
 		try {
-			s3.putObject(new PutObjectRequest(bucketName, depositId, working));
+			Upload upload = this.transferManager.upload(bucketName, depositId, working);
+			upload.waitForCompletion();
 		} catch (AmazonServiceException ase) {
 			logger.info("Caught an AmazonServiceException, which means your request made it "
                     + "to Amazon S3, but was rejected with an error response for some reason.");
