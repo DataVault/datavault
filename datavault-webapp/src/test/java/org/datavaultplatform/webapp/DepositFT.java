2febedc4f7d1135d1ee06be91bd8af4202f8f3d9
Steven Carlysle-Davies <steven.carlysle-davies@ed.ac.uk>
2018-05-30 15:10:51 +0100

    Split out first-run test

--- datavault-webapp/src/test/java/org/datavaultplatform/webapp/DepositFT.java
+++ datavault-webapp/src/test/java/org/datavaultplatform/webapp/DepositFT.java
@@ -10,36 +10,43 @@ import org.datavaultplatform.webapp.page.VaultsPage;
 
 public class DepositFT extends BaseFT {
     @Test
+    public void testDepositFirstRun() throws Exception {
+        // This test currently assumes it's the first time this user has logged-in, they have no storage or vaults defined, and so we create a local filestore and a vault called "My Vault"
+        // Any other circumstance, it'll fail, e.g. they've already created storage/vaults
+        driver.get(host + "/datavault-webapp/");
+        LoginPage page = new LoginPage(driver);
+        page = page.typeUsername("user1")
+                   .typePassword("password1");
+
+        FirstRunPage firstRunPage = page.submitLoginExpectingFirstRun();
+        firstRunPage = firstRunPage.clickDefineStorage()
+                                   .clickAddLocalFilestore()
+                                   .submitLocalFilestoreDialog()
+                                   .submit();
+
+        Assert.assertTrue(firstRunPage.isStorageDefined());
+        VaultsPage vaultsPage = firstRunPage.clickAddVault()
+                                 .addSampleVault()
+                                 .clickMyVaults();
+
+        DepositPage depositPage = vaultsPage.clickVault("My Vault")
+                                            .clickDeposit()
+                                            .depositFirstLocalFile();
+
+        depositPage.waitForStatus("Complete");
+        depositPage.logout("user1");
+    }
+
+    @Test(dependsOnMethods={"testDepositFirstRun"}, alwaysRun=true)
     public void testDeposit() throws Exception {
-        // This test currently assumes one of two things:
-        // * It's the first time this user has logged-in, they have no storage or vaults defined, and so we create a local filestore and a vault called "My Vault"
-        // * It's not the first time they've logged in and both of those things have happened already
+        // This test currently assumes it's not the first time this user has logged in and they have created a local filestore and a vault called "My Vault"
         // Any other circumstance, it'll fail, e.g. they've defined a vault with a different name, or added SFTP storage etc.
         driver.get(host + "/datavault-webapp/");
         LoginPage page = new LoginPage(driver);
         page = page.typeUsername("user1")
                    .typePassword("password1");
 
-        VaultsPage vaultsPage = null;
-        try {
-            vaultsPage = page.submitLogin();
-        }
-        catch(IllegalStateException e) {
-            FirstRunPage firstRunPage = new FirstRunPage(driver);
-            if (! firstRunPage.isStorageDefined() ) {
-                firstRunPage = firstRunPage.clickDefineStorage()
-                                           .clickAddLocalFilestore()
-                                           .submitLocalFilestoreDialog()
-                                           .submit();
-            }
-            Assert.assertTrue(firstRunPage.isStorageDefined());
-            if ( !firstRunPage.isVaultDefined() ) {
-                vaultsPage = firstRunPage.clickAddVault()
-                                         .addSampleVault()
-                                         .clickMyVaults();
-            }
-        }
-
+        VaultsPage vaultsPage = page.submitLogin();
         DepositPage depositPage = vaultsPage.clickVault("My Vault")
                                             .clickDeposit()
                                             .depositFirstLocalFile();
