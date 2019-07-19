b48c3381a8ca9a94be6657ee26f8a9362209a2aa
Rob March <rob.march@digirati.com>
2019-07-19 17:00:46 +0100

    Adds first cut of roles and permissions persistence layer

---
+++ datavault-common/src/main/java/org/datavaultplatform/common/model/dao/RoleDAO.java
@@ -0,0 +1,21 @@
+package org.datavaultplatform.common.model.dao;
+
+import org.datavaultplatform.common.model.RoleModel;
+import org.datavaultplatform.common.model.RoleType;
+
+import java.util.Collection;
+
+public interface RoleDAO {
+
+    void store(RoleModel role);
+
+    RoleModel find(String id);
+
+    Collection<RoleModel> findAll();
+
+    Collection<RoleModel> findAll(RoleType roleType);
+
+    void update(RoleModel role);
+
+    void delete(String id);
+}
