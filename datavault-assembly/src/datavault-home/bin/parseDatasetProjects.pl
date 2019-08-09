01f7f14c442813d4ee5db1aaacdd036632aa7577
David Speed <david.speed@ed.ac.uk>
2019-08-09 12:11:04 +0100

    #RSS044-92 Tidied up parseDatasetProjects.pl

--- datavault-assembly/src/datavault-home/bin/parseDatasetProjects.pl
+++ datavault-assembly/src/datavault-home/bin/parseDatasetProjects.pl
@@ -22,24 +22,17 @@ my %projectHash;
 foreach my $project ($projectDom->findnodes('/result/project')) {
    my $uuid = $project->findnodes('./@uuid')->to_literal();
    my $pureid = $project->findnodes('./@pureId')->to_literal();
-   #print $uuid . "\t" . $pureid . "\n";
    $projectHash{$uuid} = $pureid;
 }
 
-#foreach my $project (keys %projectHash) {
-#   print $project . "\t" . $projectHash{$project} . "\n";
-#}
-
 foreach my $project ($datasetDom->findnodes('/result/dataSet')) {
    my $datasetUuid = $project->findnodes('./@uuid')->to_literal();
-   #my @projectUuid = $project->findnodes('./relatedProjects/relatedProjects/@uuid')->to_literal();
    my @projectUuid = $project->findnodes('./relatedProjects/relatedProjects/@uuid');
    my $firstUuid = "";
    if (@projectUuid) {
       $firstUuid = shift @projectUuid;
    }
    if (defined $firstUuid && $firstUuid ne "") { 
-      #print "'" . $firstUuid->to_literal() . "'\n";
       if (exists $projectHash{$firstUuid->to_literal()}) {
          print $datasetUuid . "\t" . $projectHash{$firstUuid->to_literal()} . "\n";
       } else {
