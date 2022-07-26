135851794b5050b56473fd6a0d1447fb0dd7f7e8
David Hay <david.j.hay@gmail.com>
2022-07-26 22:18:17 +0100

    DV-5 JIRA RSS408-219 webapp, broker, ldap. rabbit. mysql integration (added EncryptionKeyStoreTest)
    This helps check an existing keystore file.

---
+++ datavault-common/src/test/java/org/datavaultplatform/common/crypto/EncryptionKeyStoreTest.java
@@ -0,0 +1,39 @@
+package org.datavaultplatform.common.crypto;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import javax.crypto.SecretKey;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class EncryptionKeyStoreTest {
+
+  @SneakyThrows
+  void checkKeyStore(String path, String password, String keyname) {
+      Encryption enc = new Encryption();
+      Encryption.addBouncyCastleSecurityProvider();
+      enc.setVaultEnable(false);
+      enc.setKeystoreEnable(true);
+      enc.setKeystorePath(path);
+      enc.setKeystorePassword(password);
+      enc.setVaultPrivateKeyEncryptionKeyName(keyname);
+      byte[] iv = Encryption.generateIV();
+      String randomSecret = UUID.randomUUID().toString();
+      SecretKey secret = Encryption.getSecretKeyFromKeyStore(keyname);
+      assertEquals("AES", secret.getAlgorithm());
+      assertEquals("RAW", secret.getFormat());
+      byte[] encrypted = Encryption.encryptSecret(randomSecret, null, iv);
+      byte[] decrpyted = Encryption.decryptSecret(encrypted, iv, null);
+      String decrypedSecret = new String(decrpyted, StandardCharsets.UTF_8);
+      assertEquals(randomSecret, decrypedSecret);
+  }
+
+  @Test
+  @Disabled
+  void testKeyStoreFile() {
+    checkKeyStore("/path/to/keystore.ks", "<password>", "<keyname>");
+  }
+}
