57f02e8be6ad9681c6fe18f462e95147498195b8
David Hay <david.j.hay@gmail.com>
2022-08-08 09:46:20 +0100

    dv-5 RSS408-174 - moved common RabbitMQ QueueConfig used by Worker and Broker into datavault-common

--- datavault-broker/src/main/java/org/datavaultplatform/broker/config/QueueConfig.java
+++ datavault-broker/src/main/java/org/datavaultplatform/broker/config/QueueConfig.java
@@ -1,37 +1,12 @@
 package org.datavaultplatform.broker.config;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.Queue;
-import org.springframework.beans.factory.annotation.Value;
+import org.datavaultplatform.common.config.BaseQueueConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConditionalOnExpression("${broker.rabbit.enabled:true}")
 @Slf4j
-public class QueueConfig {
-
-  public static final String WORKER_QUEUE_NAME = "${queue.name}";
-  public static final String BROKER_QUEUE_NAME = "${queue.events}";
-
-  //will create Q if it does not exist
-  @Bean
-  @ConditionalOnProperty(value="broker.define.queue.worker", havingValue = "true",  matchIfMissing = false)
-  public Queue workerQueue(@Value(WORKER_QUEUE_NAME) String workerQueueName) {
-    // Allow for priority messages so that a shutdown message can be prioritised if required.
-    Map<String, Object> args = new HashMap<>();
-    args.put("x-max-priority", 2);
-    return new Queue(workerQueueName, true, false, false, args);
-  }
-
-  //will create Q if it does not exist
-  @Bean
-  @ConditionalOnProperty(value="broker.define.queue.broker", havingValue = "true",  matchIfMissing = false)
-  public Queue brokerQueue(@Value(BROKER_QUEUE_NAME) String brokerQueueName) {
-    return new Queue(brokerQueueName, true, false, false);
-  }
+public class QueueConfig extends BaseQueueConfig  {
 }
