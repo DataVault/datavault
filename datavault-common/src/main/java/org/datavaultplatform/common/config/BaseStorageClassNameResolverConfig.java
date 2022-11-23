c836e32a9f310f338b0710941dccd2773ab730ac
David Hay <david.j.hay@gmail.com>
2022-11-23 09:11:00 +0000

    dv-5 RSS408-221 - added extra startup logging to show which SFTP Driver Class name is being used for 'org.datavaultplatform.common.storage.impl.SFTPFileSystem'

--- datavault-common/src/main/java/org/datavaultplatform/common/config/BaseStorageClassNameResolverConfig.java
+++ datavault-common/src/main/java/org/datavaultplatform/common/config/BaseStorageClassNameResolverConfig.java
@@ -1,10 +1,19 @@
 package org.datavaultplatform.common.config;
 
+import javax.annotation.PostConstruct;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.datavaultplatform.common.storage.StorageConstants;
 import org.datavaultplatform.common.util.StorageClassNameResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 
-public class BaseStorageClassNameResolverConfig {
+@Slf4j
+public class BaseStorageClassNameResolverConfig implements ApplicationListener<ApplicationReadyEvent> {
 
   @Value("${sftp.driver.use.apache.sshd:true}")
   private boolean sftpDriverUseApacheSSHD;
@@ -14,5 +23,12 @@ public class BaseStorageClassNameResolverConfig {
     return new StorageClassNameResolver(sftpDriverUseApacheSSHD);
   }
 
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    ApplicationContext ctx = event.getApplicationContext();
+    StorageClassNameResolver resolver =  ctx.getBean(StorageClassNameResolver.class);
+    String sftpDriverClassName = resolver.resolveStorageClassName(StorageConstants.SFTP_FILE_SYSTEM);
+    log.info("For SFTP[{}]using[{}]", StorageConstants.SFTP_FILE_SYSTEM, sftpDriverClassName);
+  }
 }
 
