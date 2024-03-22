87889b876528330bf36e558d9dcddcdfcc293888
David Hay <david.j.hay@gmail.com>
2024-03-22 13:22:52 +0000

    LUC070-80-Java17 - updated dependencies in pom.xml files

--- datavault-broker/src/main/java/org/datavaultplatform/broker/config/JacksonConfig.java
+++ datavault-broker/src/main/java/org/datavaultplatform/broker/config/JacksonConfig.java
@@ -1,13 +1,13 @@
 package org.datavaultplatform.broker.config;
 
-import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.context.annotation.Bean;
 
 public class JacksonConfig {
 
   @Bean
-  Hibernate5JakartaModule hibernate5JakartaModule() {
-    return new Hibernate5JakartaModule();
+  Hibernate6Module hibernate6JakartaModule() {
+    return new Hibernate6Module();
   }
 
 }
