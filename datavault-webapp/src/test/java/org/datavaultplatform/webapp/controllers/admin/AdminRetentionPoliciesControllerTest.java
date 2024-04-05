6efceddd2eec0fc8ef589923ec9a4400bc6ec9ed
David Hay <david.j.hay@gmail.com>
2024-04-05 11:15:27 +0100

    LUC070-96-SwitchToThymeleafTemplates - fixed problem when saving existing retentionpolicy

--- datavault-webapp/src/test/java/org/datavaultplatform/webapp/controllers/admin/AdminRetentionPoliciesControllerTest.java
+++ datavault-webapp/src/test/java/org/datavaultplatform/webapp/controllers/admin/AdminRetentionPoliciesControllerTest.java
@@ -1,4 +1,45 @@
+package org.datavaultplatform.webapp.controllers.admin;
+
+import lombok.SneakyThrows;
+import org.datavaultplatform.common.request.CreateRetentionPolicy;
+import org.datavaultplatform.webapp.services.RestService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.ModelMap;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+
 class AdminRetentionPoliciesControllerTest {
-  
+    
+    RestService mRestService = Mockito.mock(RestService.class);
+    
+    AdminRetentionPoliciesController controller  = new AdminRetentionPoliciesController(mRestService);
+    
+    
+    @Test
+    @SneakyThrows
+    void editRetentionPolicyIdsDoMatch() {
+
+        CreateRetentionPolicy createRetentionPolicy = new CreateRetentionPolicy();
+        createRetentionPolicy.setId(222);
+
+        String result = controller.editRetentionPolicy(createRetentionPolicy, new ModelMap(), "222", "notCancel");
+
+        Mockito.verify(mRestService).editRetentionPolicy(createRetentionPolicy);
+        assertEquals("redirect:/admin/retentionpolicies", result);
+    }
+
+    @Test
+    @SneakyThrows
+    void editRetentionPolicyIdsDoNotMatch() {
+
+        CreateRetentionPolicy createRetentionPolicy = new CreateRetentionPolicy();
+        createRetentionPolicy.setId(222);
+
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> {
+            controller.editRetentionPolicy(createRetentionPolicy, new ModelMap(), "111", "notCancel");
+        });
+        assertThat(ex).hasMessage("retentionPolicyId mismatch URL[111] form[222]");
+    }
 }
