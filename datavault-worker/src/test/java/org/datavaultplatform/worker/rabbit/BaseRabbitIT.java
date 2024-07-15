29fe5db192e039b0e1071c735f3b33fc643f87fb
David Hay <david.j.hay@gmail.com>
2024-07-15 15:07:03 +0100

    LUC070-73 RESTART-RETRIEVES - trying to track down CI/CD testcontainers problem

--- datavault-worker/src/test/java/org/datavaultplatform/worker/rabbit/BaseRabbitIT.java
+++ datavault-worker/src/test/java/org/datavaultplatform/worker/rabbit/BaseRabbitIT.java
@@ -1,13 +1,11 @@
 package org.datavaultplatform.worker.rabbit;
 
-import jakarta.annotation.PostConstruct;
 import org.datavaultplatform.common.docker.DockerImage;
 import org.datavaultplatform.common.util.UsesTestContainers;
 import org.datavaultplatform.worker.config.RabbitConfig;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -15,41 +13,50 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mockingDetails;
 
 @UsesTestContainers
+@Timeout(value = 5, unit = TimeUnit.MINUTES, threadMode  = Timeout.ThreadMode.SEPARATE_THREAD)
 public abstract class BaseRabbitIT {
+    
+    private static final Logger BASE_LOG = LoggerFactory.getLogger(BaseRabbitIT.class);
 
     public static final int HI_PRIORITY = 2;
     public static final int NORMAL_PRIORITY = 1;
-
-    @Autowired
-    AmqpAdmin amqpAdmin;
-    
-    @Autowired
-    ApplicationContext ctx;
-    
-    @Autowired
-    @Qualifier("monitorLogger")
-    Logger log;
-
     @Container
     private static final RabbitMQContainer RABBIT = new RabbitMQContainer(DockerImage.RABBIT_IMAGE_NAME)
             .withExposedPorts(5672, 15672)
             //.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(BaseRabbitIT.class)))
-            .waitingFor(Wait.forLogMessage(".*started TCP listener on.*\\n", 1));
+            .waitingFor(
+                    Wait.forLogMessage(".*started TCP listener on.*\\n", 1)
+                            .withStartupTimeout(Duration.ofMinutes(2)));
+    @Autowired
+    AmqpAdmin amqpAdmin;
+    @Autowired
+    ApplicationContext ctx;
+    @Autowired
+    @Qualifier("monitorLogger")
+    Logger log;
 
-    
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.rabbitmq.host", RABBIT::getHost);
         registry.add("spring.rabbitmq.port", RABBIT::getAmqpPort);
         registry.add("spring.rabbitmq.username", RABBIT::getAdminUsername);
         registry.add("spring.rabbitmq.password", RABBIT::getAdminPassword);
+        BASE_LOG.info("spring.rabbitmq.host [{}]", RABBIT.getHost());
+        BASE_LOG.info("spring.rabbitmq.port [{}]", RABBIT.getAmqpPort());
     }
 
     @BeforeEach
@@ -62,13 +69,33 @@ public abstract class BaseRabbitIT {
         log.info("info [{}]", info);
     }
     
-    @PostConstruct
-    void init() {
-        assertThat(RABBIT.isCreated()).isTrue();
+    private boolean isServerListening(String host, int port) {
+        Socket s = null;
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(host, port), 30_000);
+            log.info("Socket Connect to socket [{}/{}] SUCCESS", host, port);
+            return true;
+        } catch (Exception ex) {
+            log.error("Socket Connect to socket [{}/{}] FAILED", host, port);
+            return false;
+        } finally {
+            IOUtils.closeQuietly(s);
+        }
+    }
+
+    @BeforeEach
+    void checkRabbitConnection() {
+
+        assertThat(RABBIT.isCreated()).isTrue().withFailMessage(() -> "rabbit is NOT created");
+        assertThat(RABBIT.isRunning()).isTrue().withFailMessage(() -> "rabbit is NOT running");
         log.info("RABBIT HTTP URL [ {} ]", RABBIT.getHttpUrl());
         log.info("rabbit username [{}]", RABBIT.getAdminUsername());
         log.info("rabbit password [{}]", RABBIT.getAdminPassword());
         log.info("rabbit host [{}]", RABBIT.getHost());
         log.info("rabbit AMQP port [{}]", RABBIT.getAmqpPort());
+
+        // double check that we can connect via socket to rabbit before proceeding with actual tests
+        assertThat(isServerListening(RABBIT.getHost(), RABBIT.getAmqpPort())).isTrue();
     }
 }
