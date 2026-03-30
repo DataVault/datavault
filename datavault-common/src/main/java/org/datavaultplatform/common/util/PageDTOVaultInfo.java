62be83ed69e9c3dd02b66cc4085135554dedfc2f
David Hay <david.j.hay@gmail.com>
2026-03-30 16:13:40 +0100

    LUC070-238-openapi-swagger-doc-generation - updated swagger docs.

--- datavault-common/src/main/java/org/datavaultplatform/common/util/PageDTOVaultInfo.java
+++ datavault-common/src/main/java/org/datavaultplatform/common/util/PageDTOVaultInfo.java
@@ -3,6 +3,12 @@ package org.datavaultplatform.common.util;
 import org.datavaultplatform.common.response.VaultInfo;
 
 public class PageDTOVaultInfo extends PageDTO<VaultInfo> {
+
+    // Default constructor
+    public PageDTOVaultInfo() {
+        super();
+    }
+    
     // Copy constructor
     public PageDTOVaultInfo(PageDTO<VaultInfo> page) {
         this.setContent(page.getContent());
